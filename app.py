f091589add8a0fa87c34e48ad0ca38e93bbb9e86
jusolif <juliosolis77@gmail.com>
2026-08-12 14:17:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title("Especializacion Python for Analytics")
 st.sidebar.title("Parametros")
 st.write ("Elaborado por: Julio Solis")
+
+valor_inicial = st.number_input("Ingrese el valor inicial")
+valor_final = st.number_input("Ingrese el valor final")
