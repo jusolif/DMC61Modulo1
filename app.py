b4ff872ff8068de80b2e69de817de949e9239efd
jusolif <juliosolis77@gmail.com>
2026-08-23 16:30:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import streamlit as st
 
+st.image("Python_logo.png")
+
+
 st.title("Especializacion Python for Analytics")
 st.sidebar.title("Parametros")
 st.write ("Elaborado por: Julio Solis")
+
+modulos = st.sidebar.selectbox("Seleccione un módulo",["Módulo Listas","Módulo Arreglo";"Módulo Funciones"])
+
+if modulos == "Modulos Listas":
+  st.write = ("Bienvenido al Módulo Listas")
 
 valor_inicial = st.number_input("Ingrese el valor inicial")
 valor_final = st.number_input("Ingrese el valor final")
